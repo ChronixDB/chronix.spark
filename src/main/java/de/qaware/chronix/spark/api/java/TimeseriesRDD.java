edc71bb91859f1156bbd13cde940c86b61d47e12
Josef Adersberger <josef.adersberger@qaware.de>
2016-03-23 00:23:28 +0100

    Created project structure

---
+++ src/main/java/de/qaware/chronix/spark/api/java/TimeseriesRDD.java
@@ -0,0 +1,11 @@
+/**
+ * _____________________________________________________________________________
+ * Project   : Chronix Spark
+ * License   : Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * Designed and handcrafted in Germany, (c) 2016 QAware GmbH
+ * _____________________________________________________________________________
+ */
+package de.qaware.chronix.spark.api.java;
+
+public class TimeseriesRDD {
+}
