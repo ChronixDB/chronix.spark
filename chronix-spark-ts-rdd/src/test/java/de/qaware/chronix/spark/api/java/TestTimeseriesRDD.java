4bd96798ef9ec13db293a8741bcf2fc9e4f21b7c
Florian Lautenschlager <florian.lautenschlager@qaware.de>
2016-03-23 09:18:16 +0100

    Refactoring, and integrations for:  - Travis  - Coveralls  - Bintray

---
+++ chronix-spark-ts-rdd/src/test/java/de/qaware/chronix/spark/api/java/TestTimeseriesRDD.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2016 QAware GmbH
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+/**
+ * _____________________________________________________________________________
+ * Project   : Chronix Spark
+ * License   : Apache License 2.0 (https://opensource.org/licenses/Apache-2.0)
+ * Designed and handcrafted in Germany, (c) 2016 QAware GmbH
+ * _____________________________________________________________________________
+ */
+package de.qaware.chronix.spark.api.java;
+
+import org.junit.Test;
+
+public class TestTimeseriesRDD {
+
+    @Test
+    public void testConstructor() {
+        TimeseriesRDD tsRdd = new TimeseriesRDD();
+    }
+}
