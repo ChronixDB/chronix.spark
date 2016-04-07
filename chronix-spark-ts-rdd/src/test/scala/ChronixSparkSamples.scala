a183e6d12d6a90a3b033fb1f422bb1bf81fd02a7
Josef Adersberger <josef.adersberger@qaware.de>
2016-04-07 22:29:38 +0200

    Sample Scala Chronix Spark client code

---
+++ chronix-spark-ts-rdd/src/test/scala/ChronixSparkSamples.scala
@@ -0,0 +1,30 @@
+import de.qaware.chronix.spark.api.java.TestConfiguration._
+import de.qaware.chronix.spark.api.java.ChronixSparkContext
+import org.apache.solr.client.solrj.{SolrQuery, SolrServerException}
+import org.apache.spark.SparkConf
+import org.apache.spark.api.java.{JavaRDD, JavaSparkContext}
+
+
+object ChronixSparkSamples {
+  def main(args: Array[String]): Unit = {
+    val conf: SparkConf = new SparkConf().setMaster(SPARK_MASTER).setAppName(APP_NAME)
+    try {
+      val sc: JavaSparkContext = new JavaSparkContext(conf)
+      try {
+        val csc: ChronixSparkContext = new ChronixSparkContext(sc)
+        val query: SolrQuery = new SolrQuery(
+          "metric:\"MXBean(java.lang:type=Memory).NonHeapMemoryUsage.used\" AND type:RECORD")
+        val result = csc.queryChronix(query, ZK_HOST)
+        val timeSeries = result.take(5)
+        println(timeSeries)
+      }
+      catch {
+        case e: SolrServerException => {
+          throw new RuntimeException(e)
+        }
+      } finally {
+        if (sc != null) sc.close()
+      }
+    }
+  }
+}
