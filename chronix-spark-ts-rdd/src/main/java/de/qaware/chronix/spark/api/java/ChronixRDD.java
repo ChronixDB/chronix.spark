2aa9631944f0f2040d7fb63e4a32d9b1093fd13e
Josef Adersberger <josef.adersberger@qaware.de>
2016-04-08 15:39:12 +0200

    Enhanced ChronixRDD with several actions and transformations

--- chronix-spark-ts-rdd/src/main/java/de/qaware/chronix/spark/api/java/ChronixRDD.java
+++ chronix-spark-ts-rdd/src/main/java/de/qaware/chronix/spark/api/java/ChronixRDD.java
@@ -16,10 +16,18 @@
 package de.qaware.chronix.spark.api.java;
 
 import de.qaware.chronix.timeseries.MetricTimeSeries;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.spark.api.java.JavaDoubleRDD;
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.DoubleFlatMapFunction;
+import org.apache.spark.api.java.function.DoubleFunction;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.MapFunction;
 import scala.reflect.ClassTag$;
 
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * ChronixRDD: A time series implementation based on Chronix.
@@ -47,20 +55,76 @@ public class ChronixRDD extends JavaRDD<MetricTimeSeries> {
 
 
     /**
-     * Action: Compute the average value.
+     * Action: Compute the mean value.
      *
-     * @return average value over all contained time series.
+     * @return mean value over all contained time series.
      */
-    public float avg() {
-        return 0.0f;
+    public double mean() {
+        return getValuesAsRdd().mean().doubleValue();
     }
 
-    //max()
-    //min()
-    //delta()
-    //filter()
-    //see TimeSeriesRDD
-    //see Kassiopeia TimeSeries POJO
+    /**
+     * Action: Compute the approximate mean value.
+     *
+     * @return approximate mean value over all contained time series.
+     */
+    public double approxMean() {
+        return getValuesAsRdd().meanApprox(1000).getFinalValue().mean();
+    }
 
+    /**
+     * Action: Compute the min value.
+     *
+     * @return min value over all contained time series.
+     */
+    public double min() {
+        return getValuesAsRdd().min().doubleValue();
+    }
 
+    /**
+     * Action: Compute the max value.
+     *
+     * @return max value over all contained time series.
+     */
+    public double max() {
+        return getValuesAsRdd().max().doubleValue();
+    }
+
+    /**
+     * Transformation: Get all values as JavaDoubleRDD.
+     *
+     * @return a RDD with all observation values
+     */
+    public JavaDoubleRDD getValuesAsRdd() {
+        return this.flatMapToDouble(
+                new DoubleFlatMapFunction<MetricTimeSeries>() {
+                    @Override
+                    public Iterable<Double> call(MetricTimeSeries mts) throws Exception {
+                        return Arrays.asList(ArrayUtils.toObject(mts.getValuesAsArray()));
+                    }
+                });
+    }
+
+    /**
+     * Action: Counts the number of observations.
+     *
+     * @return the number of overall observations in all time series
+     */
+    public long countObservations() {
+        JavaDoubleRDD sizesRdd = this.mapToDouble(
+                new DoubleFunction<MetricTimeSeries>() {
+                    @Override
+                    public double call(MetricTimeSeries value) throws Exception {
+                        return (double)value.size();
+                    }
+                });
+        return sizesRdd.sum().longValue();
+    }
+
+    //TODO:
+    // - delta()
+    // - filter()
+    // - easy additional functions based on DoubleRDD (esp. stats & approx)
+    // - see TimeSeriesRDD
+    // - see Kassiopeia TimeSeries POJO
 }
--- chronix-spark-ts-rdd/src/test/groovy/de/qaware/chronix/spark/api/java/TestChronixRDD.groovy
+++ chronix-spark-ts-rdd/src/test/groovy/de/qaware/chronix/spark/api/java/TestChronixRDD.groovy
@@ -19,27 +19,91 @@ import de.qaware.chronix.timeseries.MetricTimeSeries
 import org.apache.solr.client.solrj.SolrQuery
 import org.apache.spark.SparkConf
 import org.apache.spark.api.java.JavaSparkContext
+import spock.lang.Ignore
 import spock.lang.Specification
 
 class TestChronixRDD extends Specification {
 
-    def "test constructor"() {
+    @Ignore
+    def "test iterator"() {
         given:
         SparkConf conf = new SparkConf().setMaster(ConfigurationParams.SPARK_MASTER).setAppName(ConfigurationParams.APP_NAME);
         JavaSparkContext sc = new JavaSparkContext(conf)
         ChronixSparkContext csc = new ChronixSparkContext(sc);
         SolrQuery query = new SolrQuery("metric:\"MXBean(java.lang:type=Memory).NonHeapMemoryUsage.used\" AND type:RECORD");
         ChronixRDD rdd = csc.queryChronix(query, ConfigurationParams.ZK_HOST);
+        when:
         Iterator<MetricTimeSeries> it = rdd.iterator();
         int i = 0;
-
-        when:
         while (it.hasNext()) {
             it.next();
             i++;
         }
         then:
         i > 0
+        cleanup:
+        sc.close()
+    }
+
+    @Ignore
+    def "test mean"() {
+        given:
+        SparkConf conf = new SparkConf().setMaster(ConfigurationParams.SPARK_MASTER).setAppName(ConfigurationParams.APP_NAME);
+        JavaSparkContext sc = new JavaSparkContext(conf)
+        ChronixSparkContext csc = new ChronixSparkContext(sc);
+        SolrQuery query = new SolrQuery("metric:\"MXBean(java.lang:type=Memory).NonHeapMemoryUsage.used\" AND type:RECORD");
+        ChronixRDD rdd = csc.queryChronix(query, ConfigurationParams.ZK_HOST);
+        when:
+        long start = System.currentTimeMillis();
+        double mean = rdd.mean();
+        long stop = System.currentTimeMillis();
+        println "Mean: " + mean
+        println "   - execution time: " + (stop - start)
+        then:
+        mean > 0.0f
+        cleanup:
+        sc.close()
+    }
+
+    @Ignore
+    def "test approx. mean"() {
+        given:
+        SparkConf conf = new SparkConf().setMaster(ConfigurationParams.SPARK_MASTER).setAppName(ConfigurationParams.APP_NAME);
+        JavaSparkContext sc = new JavaSparkContext(conf)
+        ChronixSparkContext csc = new ChronixSparkContext(sc);
+        SolrQuery query = new SolrQuery("metric:\"MXBean(java.lang:type=Memory).NonHeapMemoryUsage.used\" AND type:RECORD");
+        ChronixRDD rdd = csc.queryChronix(query, ConfigurationParams.ZK_HOST);
+        when:
+        long start = System.currentTimeMillis();
+        double mean = rdd.approxMean()
+        long stop = System.currentTimeMillis();
+        println "Approx. mean: " + mean
+        println "   - execution time: " + (stop - start)
+        then:
+        mean > 0.0f
+        cleanup:
+        sc.close()
+    }
+
+    @Ignore
+    def "test scalar actions"() {
+        given:
+        SparkConf conf = new SparkConf().setMaster(ConfigurationParams.SPARK_MASTER).setAppName(ConfigurationParams.APP_NAME);
+        JavaSparkContext sc = new JavaSparkContext(conf)
+        ChronixSparkContext csc = new ChronixSparkContext(sc);
+        SolrQuery query = new SolrQuery("metric:\"MXBean(java.lang:type=Memory).NonHeapMemoryUsage.used\" AND type:RECORD");
+        ChronixRDD rdd = csc.queryChronix(query, ConfigurationParams.ZK_HOST);
+        when:
+        long count = rdd.countObservations()
+        double max = rdd.max()
+        double min = rdd.min()
+        println "Count: " + count
+        println "Max: " + max
+        println "Min: " + min
+        then:
+        count > 0
+        cleanup:
+        sc.close()
     }
 
 }
